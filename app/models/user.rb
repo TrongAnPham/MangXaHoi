6f453ba1d7ce72b7fab99ab4838d56d00822fdce
NguyenhoangDuy1123 <nghduy11011995@gmail.com>
2016-10-05 22:47:08 +0700

    Tạo model user
    _ model đã có user name + ràng buộc
    _ model đã có email + ràng buộc
    _ model đã có password + ràng buộc + mã hóa bằng bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
 	attr_accessor :name, :email, :password
+	
+	validates(:name, presence: true, length: { maximum: 50 })
+	validates(:email, presence: true, length: { maximum: 50 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true)
+	validates(:password, presence: true, length: { minimum: 8 })
+	
+	has_secure_password
 end
