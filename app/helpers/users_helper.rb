2aea81648febe119743a5e07d607c71c258166c7
NguyenhoangDuy1123 <nghduy11011995@gmail.com>
2016-10-07 08:46:53 +0700

    Lỗi
    Lỗi

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,8 @@
+module UsersHelper
+	def avatar_for(user, options = { size: 50 })
+		avatar_id = Digest::MD5::hexdigest(user.email)
+		size = options[:size]
+		avatar_url = "https://secure.gravatar.com/avatar/#{avatar_id}?s=#{size}"
+		image_tag(avatar_url, alt: user.name, class: "gravatar")
+  end
+end
