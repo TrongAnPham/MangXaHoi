88abf308bec09adb1907052b4c17e1cc6349443b
NguyenhoangDuy1123 <nghduy11011995@gmail.com>
2016-10-07 23:51:50 +0700

    Cài đặt chức năng đăng ký
    Người dùng đã có thể đăng ký

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
-	def avatar_for(user, options = { size: 50 })
+	def avatar_for(user, options = { size: 100 })
 		avatar_id = Digest::MD5::hexdigest(user.email)
 		size = options[:size]
 		avatar_url = "https://secure.gravatar.com/avatar/#{avatar_id}?s=#{size}"
