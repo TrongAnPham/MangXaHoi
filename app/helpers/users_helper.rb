234144c4bdd6fa69fed887cfd6fce462005a3207
NguyenhoangDuy1123 <nghduy11011995@gmail.com>
2016-10-07 07:44:00 +0700

    Update avatar cho user
    _ Sử dụng gravatar  để làm avatar cho user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+	def avatar_for(user)
+		avatar_id = Digest::MD5::hexdigest(user.email)
+		avatar_url = "https://secure.gravatar.com/avatar/#{avatar_id}"
+		image_tag(avatar_url, alt: user.name, class: "gravatar")
+  end
 end
