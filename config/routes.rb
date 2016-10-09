9836e7ce3d01c556bf4e8d3dab74c5a182c5e8a9
NguyenhoangDuy1123 <nghduy11011995@gmail.com>
2016-10-09 23:49:37 +0700

    Tính năng đăng nhập
    _ Người dùng có thể đăng nhập vào ứng dụng
    _ Tình trạng đang nhập được lưu cho tới khi người dùng log out hay tắt
    broswer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :users
   
-  root 'users#index'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
+  root 'sessions#root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
